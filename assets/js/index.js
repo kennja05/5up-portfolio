cb620d71746b1ea7ab75900d60036613108d8559
kennja05 <jacobkenny05@gmail.com>
2020-08-14 13:25:50 -0400

    update description

--- assets/js/index.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("scroll", () => {
-    rotate()
-})
-
-
-function rotate(){
-    const toggler = document.getElementById('rotate')
-    toggler.style.transform = "rotate(" + window.pageYOffset/2 + "deg)"
-}
