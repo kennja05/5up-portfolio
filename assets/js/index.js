d3e450203b7d4f7f55e2e64304ef1455f3b92421
kennja05 <jacobkenny05@gmail.com>
2020-08-15 16:51:55 -0400

    add additional cog

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,4 +1,7 @@
 window.addEventListener("scroll", () => {
-    let cog = document.getElementById('cog')
-    cog.style.transform = `rotate(${pageYOffset/2}deg)`
+    let cogs = Array.from(document.getElementsByClassName('cog'))
+    cogs.forEach((cog, index) => {
+        let sign = index % 2 === 0 ? -1 : 1
+        cog.style.transform = `rotate(${sign * pageYOffset/1.5}deg)`
+    })
 })
