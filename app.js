12104ae70748d8d36e65036497a1540ed241e2f9
kennja05 <jacobkenny05@gmail.com>
2020-09-19 00:11:46 -0400

    add 404

--- app.js
+++ app.js
@@ -1,13 +1,22 @@
 // create an express app
 const express = require("express")
+
+const path = require('path')
 const app = express()
 
 // use the express-static middleware
 app.use(express.static("public"))
 
-// define the first route
-app.get("/", function (req, res) {
-  res.send("<h1>Hello World!</h1>")
+app.get('/', function(req, res){
+  res.sendFile(path.join(__dirname + '/public/index.html'))
+})
+
+app.get('*', function(req, res){
+  res.sendFile(path.join(__dirname + '/public/404.html'))
+})
+
+app.use(function (req, res, next){
+  res.status(404).sendFile(path.join(__dirname + '/public/404.html'))
 })
 
 // start the server listening for requests
